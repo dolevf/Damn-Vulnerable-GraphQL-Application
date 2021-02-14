bda63384fdb3f9e2ddb8be40a707e76e508c676d
D. Connor McKinnon <mckinnondconnor@gmail.com>
2021-02-14 15:30:05 -0500

    improve solution layouts

--- db/solutions.py
+++ db/solutions.py
@@ -1,17 +1,1 @@
-solutions = [
-    "partials/solutions/solution_1.html",
-    "partials/solutions/solution_2.html",
-    "partials/solutions/solution_3.html",
-    "partials/solutions/solution_4.html",
-    "partials/solutions/solution_5.html",
-    "partials/solutions/solution_6.html",
-    "partials/solutions/solution_7.html",
-    "partials/solutions/solution_8.html",
-    "partials/solutions/solution_9.html",
-    "partials/solutions/solution_10.html",
-    "partials/solutions/solution_11.html",
-    "partials/solutions/solution_12.html",
-    "partials/solutions/solution_13.html",
-    "partials/solutions/solution_14.html",
-    "partials/solutions/solution_15.html",
-]
+solutions = list(range(1,16))
